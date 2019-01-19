ac62a440f291e1bbd314682b89586d5376cba7dd
Marco Fincato <marcofincato777@gmail.com>
2019-01-19 19:35:13 +0100

    Added simple expression evaluation.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,34 @@
 
 #define RESOLUTION 512
 
+using namespace std;
+
+typedef exprtk::symbol_table<double> Symbols;
+typedef exprtk::expression<double> Expression;
+typedef exprtk::parser<double> Parser;
+
+void compute() {
+    string expression_string = "x + 2";
+    double x;
+
+    Symbols symbol_table;
+    symbol_table.add_variable("x",x);
+    symbol_table.add_constants();
+
+    Expression expression;
+    expression.register_symbol_table(symbol_table);
+
+    Parser parser;
+    parser.compile(expression_string,expression);
+
+    for (x = double(-5); x <= double(+5); x += double(1)) {
+        double y = expression.value();
+        cout << "X: " << x << "\tY: " << y << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    compute();
     return 0;
 }
 
